8eb15ce758f9f603304844d8b5de8592c53b3208
SebastianGaleano20 <sebascarp9@gmail.com>
2025-05-26 18:51:56 -0300

    schemaValidator added

---
+++ src/middlewares/schemaValidator.js
@@ -0,0 +1,14 @@
+import HTTP_STATUS from "../helpers/httpStatus.js";
+
+// Función para validar schema de datos ingresados por el usuario
+export const schemaValidator = (schema) => {
+  // Recibimos el schema a validar
+  return (req, res, next) => {
+    const { error } = schema.validate(req.body, { abortEarly: false }); // Capturamos el error
+    if (error) {
+      const errorMessage = error.details.map((err) => err.message).join(", ");
+      res.status(HTTP_STATUS.BAD_REQUEST).json({ error: errorMessage });
+    }
+    next(); // Si no hay error sigue con el controlador
+  };
+};
