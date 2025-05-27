92893104fa1a785b35248bef2b7bb89fa4658bff
SebastianGaleano20 <sebascarp9@gmail.com>
2025-05-27 00:37:47 -0300

    errorHandler.js added

--- src/index.js
+++ src/index.js
@@ -2,13 +2,29 @@
 import dotenv from "dotenv";
 // express como dependencia para levantar servidor
 import express from "express";
-
+// cors para configurar ENDPOINTS disponibles para utilizar en nuestra API
+import cors from "cors";
+// app contiene rutas de la API a crear
+import app from "./app.js";
+// Manejador de errores
+import errorHandler from "./middlewares/errorHandler.js";
 // Iniciamos dotenv
 dotenv.config();
 // Instanciamos con process.env.SERVER_PORT a la variable de entorno
 const SERVER_PORT = process.env.SERVER_PORT || 3001;
 // Iniciamos express
 const server = express();
+// Configuración de cors para habilitar metodos
+server.use(
+  cors({
+    origin: "*",
+    methods: "GET, POST, PUT, DELETE",
+  })
+);
+// Indicamos '/api' como ruta para app
+server.use("/api", app);
+// Utilizamos nuestro manejador de errores
+server.use(errorHandler);
 // Servidor ON
 server.listen(SERVER_PORT, () => {
   console.log(`Server running on port ${SERVER_PORT}`);
