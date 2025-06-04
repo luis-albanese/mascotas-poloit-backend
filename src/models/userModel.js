fa38be894b6e8112fd65786a96bd1cca012c952f
SebastianGaleano20 <sebascarp9@gmail.com>
2025-06-04 14:59:11 -0300

    Encrypt use in createUser

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,8 +1,15 @@
 import prisma from "../config/prisma";
+import { encrypt } from "../utils/bcrypt";
 
 export const userModel = () => {
   // Model para crear usuario
   const createUser = async (data) => {
+    // Extraemos la contraseña
+    const password = data.password;
+    // Encriptamos la contraseña
+    const hash = await encrypt(password);
+    // Guardamos en data la contraseña encriptada
+    data.password = hash;
     return await prisma.user.create({
       data,
     });
