cff1b1872e12c75dd8f7c212611f78c1ea84711c
SebastianGaleano20 <sebascarp9@gmail.com>
2025-06-04 16:23:03 -0300

    userController - added validations

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,16 +4,28 @@ import createUserSchema from "../schemas/createUserSchema.js";
 
 export const userController = () => {
   // Extraemos funciones del Service
-  const { create } = userService();
+  const { create, findUser } = userService();
   // Controlador para crear nuevo usuario
   const newUser = async (req, res, next) => {
     // Extraemos body
     const { body } = req;
+    // Validamos los datos
     const { error: validationError } = createUserSchema.validate(body);
+    // Si existe error, enviamos respuesta al usuario
     if (validationError) {
       return res
         .status(HTTP_STATUS.BAD_REQUEST)
         .json({ error: validationError.details[0].message });
+    }
+    // Extraemos el email del body
+    const { email } = body;
+
+    // Verificamos si existe el usuario
+    const existingUser = await findUser(email);
+    if (existingUser) {
+      return res
+        .status(HTTP_STATUS.BAD_REQUEST)
+        .json({ error: "User already exists" });
     }
     try {
       // Creamos nuevo usuario
