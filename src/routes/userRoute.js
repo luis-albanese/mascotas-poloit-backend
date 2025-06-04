0f268afa1d926e1d5b222b3cee788cded341ab0d
SebastianGaleano20 <sebascarp9@gmail.com>
2025-06-04 17:00:59 -0300

    userRoute - get added

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,8 +1,14 @@
 import { Router } from "express";
 import { userController } from "../controllers/userController.js";
+
 export const userRoutes = () => {
+  // Iniciamos Router de express
   const userRoute = Router();
-  const { newUser } = userController();
+  // Extraemos funciones del Controller
+  const { newUser, getUsers } = userController();
+  // Ruta para crear usuario
   userRoute.post("/new", newUser);
+  // Ruta para obtener usuarios
+  userRoute.get("/", getUsers);
   return userRoute;
 };
