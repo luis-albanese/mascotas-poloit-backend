3ff241a39898651ed2273bae19ba11356da3f8e9
SebastianGaleano20 <sebascarp9@gmail.com>
2025-06-13 12:51:44 -0300

    petService - getPets created

--- src/services/petService.js
+++ src/services/petService.js
@@ -1,11 +1,19 @@
 import { petModel } from "../models/petModel";
 
 export const petService = () => {
-  const { createPet } = petModel();
+  const { createPet, getAllPets } = petModel();
   const create = async (data) => {
     try {
       const res = await createPet(data);
       return res;
+    } catch (error) {
+      return error;
+    }
+  };
+  const getPets = async () => {
+    try {
+      const pets = await getAllPets();
+      return pets;
     } catch (error) {
       return error;
     }
