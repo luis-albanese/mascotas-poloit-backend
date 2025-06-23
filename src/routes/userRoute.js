e6cc581fe153fa40db0514e74956755cce0ec191
SebastianGaleano20 <sebascarp9@gmail.com>
2025-06-23 20:54:24 -0300

    userRoute - login and refreshToken added

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -5,13 +5,18 @@ export const userRoutes = () => {
   // Iniciamos Router de express
   const userRoute = Router();
   // Extraemos funciones del Controller
-  const { newUser, getUsers, updateDataUser } = userController();
+  const { newUser, getUsers, updateDataUser, loginUser, refreshToken } =
+    userController();
   // Ruta para crear usuario
   userRoute.route("/create").post(newUser);
   // Ruta para obtener usuarios
   userRoute.route("/").get(getUsers);
   // Ruta para actualizar usuario
   userRoute.route("/:id").patch(updateDataUser);
+  // Ruta para iniciar sesión
+  userRoute.route("/login").post(loginUser);
+  // Ruta para refrescar token
+  userRoute.route("/refresh-token").post(refreshToken);
   // Devolvemos el router
   return userRoute;
 };
